223706294ba87576d497bcddf6e65f8ba82ff983
DK <david@backstrip.net>
2024-10-18 15:51:44 +1100

    Remove Evil

--- init-lsp.el
+++ init-lsp.el
@@ -1,26 +1,49 @@
-;; LSP
+;; LSP configuration
 (use-package lsp-mode
-  :hook ((csharp-mode . lsp-deferred)
-	 (asm-mode . lsp-deferred)))
+  :hook (csharp-mode . lsp-deferred)
+  :commands lsp)
 
-(use-package flycheck)
+(use-package csharp-mode
+  :ensure t
+  :mode "\\.cs\\'")
 
 (use-package lsp-ui
   :ensure t
   :after lsp-mode
   :hook (lsp-mode . lsp-ui-mode)
   :config
-  ;; Enable sideline for diagnostics only
-  (setq lsp-ui-sideline-enable t
-        lsp-ui-sideline-show-hover nil        ;; Disable hover information
-        lsp-ui-sideline-show-symbol nil       ;; Disable symbol information
-        lsp-ui-sideline-show-diagnostics t    ;; Enable diagnostics display
+  ;; Customize lsp-ui behavior
+  (setq lsp-ui-sideline-enable nil
+        lsp-ui-sideline-show-hover nil     
+        lsp-ui-sideline-show-symbol nil    
+        lsp-ui-sideline-show-diagnostics t 
         lsp-ui-sideline-update-mode 'point
-
-        ;; Disable documentation popups if not needed
-        lsp-ui-doc-enable nil
-       ) 
-
-  ;; Optional: Customize the appearance
-  (setq lsp-ui-doc-border "gray"
+        lsp-ui-doc-enable nil              
+        lsp-ui-doc-border "gray"
         lsp-ui-sideline-ignore-duplicate t))
+
+;; General LSP settings
+(setq lsp-response-timeout 30
+      lsp-log-io nil
+      lsp-diagnostics-provider :flycheck
+      flycheck-checker-error-threshold 1000
+      gc-cons-threshold 100000000
+      read-process-output-max (* 1024 1024)     ;; 1MB
+      lsp-idle-delay 0.5
+;;      lsp-csharp-omnisharp-enable-decompilation-support t
+      lsp-completion-show-kind t)
+
+;; Company-mode for autocompletion
+(use-package company
+  :after lsp-mode
+  :hook (prog-mode . company-mode)
+  :bind (:map lsp-mode-map
+              ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+;; More detailed company completion box
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
--- init.el
+++ init.el
@@ -22,26 +22,60 @@
 (setq use-package-always-ensure t)
 
 ;; Make emacs normal
+
+;; General settings
+(setq inhibit-startup-message t
+      initial-scratch-message ";; scratch\n\n"
+      split-height-threshold nil
+      split-width-threshold 80
+      ring-bell-function 'ignore
+      scroll-margin 7)
+
 (fset 'yes-or-no-p 'y-or-n-p)
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message ";; scratch\n\n")
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(setq split-height-threshold nil)
-(setq split-width-threshold 80)
-(setq ring-bell-function 'ignore)
 (delete-selection-mode 1)
-(global-set-key (kbd "C-c C-a") 'mark-whole-buffer)
-(global-set-key (kbd "C-c a") 'mark-whole-buffer)
-(global-set-key (kbd "C-]") 'counsel-M-x)
 
+;; Disable UI elements
+(dolist (mode '(scroll-bar-mode tool-bar-mode menu-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+
+;; General key bindings
+(use-package emacs
+  :bind (("<escape>" . keyboard-escape-quit)
+         ("C-c C-a" . mark-whole-buffer)
+         ("C-c a" . mark-whole-buffer)
+         ("C-\\" . counsel-M-x)
+         ("C-]" . counsel-projectile-switch-to-buffer)
+	 ("C-c b" . counsel-switch-buffer)
+	 ("C-c SPC" . counsel-buffer-or-recentf)
+         ("C-." . forward-word)
+         ("C-," . backward-word)
+         ("C->" . scroll-up)
+         ("C-<" . scroll-down)
+         ("M-]" . forward-paragraph)
+         ("M-[" . backward-paragraph)
+	 ("C-c o" . delete-other-windows)
+	 ("C-c 0" . delete-window)
+         ("C-c ]" . next-buffer)
+         ("C-c [" . previous-buffer)
+         ("C-;" . goto-line)))
+
+;; Make sure M-x is always available
 (use-package bind-key
-  :config
-  (bind-key* "C-]" 'counsel-M-x))
+   :config
+   (bind-key* "C-\\" 'counsel-M-x))
 
-;; Automatically switch to new window after vsplit
+;; Remove noise from mode-line, except for
+;; the specied ones
+(use-package minions
+  :config
+  (minions-mode 1)
+  (add-to-list 'minions-prominent-modes 'flycheck-mode)
+  (add-to-list 'minions-prominent-modes 'lsp-mode))
+
+;; Remove line number from mode-line
+(line-number-mode 0)
+
+;; Automatically switch to new window after splits
 (defun split-and-follow-horizontally ()
   (interactive)
   (split-window-below)
@@ -49,6 +83,12 @@
   (other-window 1))
 (global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
 
+(defun split-and-follow-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
 ;; Automatically focus on compilation buffer
 (defadvice compile (after switch-to-compile-buffer activate)
@@ -81,9 +121,13 @@
 			    "#707070")))
   (set-face-foreground current-linum-face less-dimmed-color))
 
+
 ;; Basic config
-(global-display-line-numbers-mode t)
-(setq display-line-numbers 'relative)
+
+;; Display line numbers in these modes
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Highlight current line
 (global-hl-line-mode 1)
 
 (use-package ace-window
@@ -93,11 +137,18 @@
 (use-package multiple-cursors
   :bind (("C-c C-c" . mc/edit-lines)))
 
+;; Centres buffer on a widescreen
+(use-package olivetti
+  :init
+  (setq olivetti-body-width 100)
+  :hook ((text-mode . olivetti-mode)
+         (prog-mode . olivetti-mode)
+         (org-mode . olivetti-mode)))
+
 ;; search, narrowing
 (use-package counsel
   :init (ivy-mode 1)
   :bind (("M-x" . counsel-M-x)
-	 ("C-c C-SPC" . counsel-buffer-or-recentf)
 	 ("C-c SPC" . counsel-buffer-or-recentf)))
 
 (use-package smex)
@@ -112,6 +163,9 @@
  '(avy-lead-face-1 ((t (:foreground "black" :background "green"))))
  '(avy-lead-face-2 ((t (:foreground "white" :background "blue")))))
 
+(use-package avy-zap
+  :bind (("C-M-'" . avy-zap-up-to-char)))
+
 (use-package projectile
   :init (projectile-mode 1))
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
@@ -120,7 +174,13 @@
 (use-package counsel-projectile)
 
 (use-package company
-  :config (add-hook 'after-init-hook 'global-company-mode))
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection))
+  :config
+(add-hook 'prog-mode-hook 'company-mode)
+(setq company-format-margin-function nil))
+
+
 
 (use-package which-key
   :ensure t
@@ -131,6 +191,8 @@
 
 (use-package company-prescient
   :init (company-prescient-mode))
+
+(use-package flycheck)
 
 ;; Git
 (use-package magit)
@@ -146,4 +208,7 @@
 ;;(load-file (concat user-emacs-directory "init-wsl.el"))
 
 ;; LSP
-;;(load-file (concat user-emacs-directory "init-lsp.el"))
+(load-file (concat user-emacs-directory "init-lsp.el"))
+
+;; posframe, like ST or VSC's omnipanel
+(load-file (concat user-emacs-directory "init-posframe.el"))
