40a995fd02224592095879d4b40ff2f3ee0b5943
DK <david@backstrip.net>
2024-12-04 17:03:02 +1100

    Add C specific configuration

--- init-lsp.el
+++ init-lsp.el
@@ -1,49 +1,63 @@
-;; Make sure this is setup
 (setq gc-cons-threshold 100000000)
 
-;; LSP configuration
 (use-package lsp-mode
-  :hook (csharp-mode . lsp-deferred)
+  :hook (
+         (csharp-mode . lsp-deferred)
+         (c-mode . lsp-deferred)
+        )
   :bind (("C-c c d" . lsp-find-definition)
-	 ("C-c c e" . flycheck-list-errors)
-	 ("C-c c r" . lsp-find-references)
-	 ("C-c c l" . lsp-lens-mode))
-
-  :custom (lsp-auto-guess-root nil)
-  (lsp-prefer-flymake nil) 
+         ("C-c c e" . flycheck-list-errors)
+         ("C-c c r" . lsp-find-references)
+         ("C-c c l" . lsp-lens-mode))
+  :custom
+  (lsp-auto-guess-root nil)
+  (lsp-prefer-flymake nil)
   (lsp-enable-file-watchers nil)
   (lsp-enable-folding nil)
   (lsp-lens-enable nil)
   (lsp-lens-place-position 'above)
-  (read-process-output-max (* 1024 1024))
+  (read-process-output-max (* 1024 1024)) 
   (lsp-keep-workspace-alive nil)
   (lsp-eldoc-hook nil)
   (setq lsp-response-timeout 30
         lsp-log-io nil
         lsp-diagnostics-provider :flycheck
         flycheck-checker-error-threshold 1000
-        read-process-output-max (* 1024 1024)  ;; 1MB
+        read-process-output-max (* 1024 1024) 
         lsp-idle-delay 0.5
         lsp-completion-show-kind t)
   :commands lsp)
 
 (use-package csharp-mode
   :ensure t
-  :mode "\\.cs\\'")
+  :mode "\\.cs\\'"
+  :hook (csharp-mode . (lambda ()
+                          (setq c-basic-offset 4
+                                tab-width 4
+                                indent-tabs-mode nil))))
+
+(use-package cc-mode
+  :ensure t
+  :mode ("\\.c\\'" . c-mode)
+  :mode ("\\.h\\'" . c-mode) 
+  :hook (c-mode . (lambda ()
+                    (setq c-basic-offset 2
+                          tab-width 2
+                          indent-tabs-mode nil))))
 
 (use-package tree-sitter
   :ensure t
-  :hook (csharp-ts-mode . tree-sitter-hl-mode))
+  :hook ((csharp-ts-mode c-ts-mode) . tree-sitter-hl-mode))
 
 (use-package tree-sitter-langs
   :ensure t)
 
+;; LSP UI
 (use-package lsp-ui
   :ensure t
   :after lsp-mode
   :hook (lsp-mode . lsp-ui-mode)
   :config
-  ;; Customize lsp-ui behavior
   (setq lsp-ui-sideline-enable nil
         lsp-ui-sideline-show-hover nil     
         lsp-ui-sideline-show-symbol nil    
@@ -58,12 +72,7 @@
   '(flycheck-fringe-info ((t (:foreground "darkorange")))))
 
 (setq-default c-basic-offset 4)
-(add-hook 'csharp-mode-hook
-          (lambda ()
-            (setq c-basic-offset 4
-                  tab-width 4
-                  indent-tabs-mode nil)))
-;;Company-mode for autocompletion		
+
 (use-package company
   :after lsp-mode
   :hook (prog-mode . company-mode)
@@ -73,6 +82,8 @@
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
 
-;;More detailed company completion box
 (use-package company-box
   :hook (company-mode . company-box-mode))
+
+;; Uncomment and set the path to clangd if necessary
+;; (setq lsp-clients-clangd-executable "/path/to/clangd")
