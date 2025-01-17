770a4d55d816fd0a151dac5a23ddbe33cacfdeff
David Kidd <david@backstrip.net>
2025-01-17 13:36:59 +1100

    Tweak function name highlight

--- custom/novarange-theme.el
+++ custom/novarange-theme.el
@@ -59,7 +59,7 @@
 	 `(font-lock-reference-face ((,class (:foreground ,const))))
 	 `(font-lock-constant-face ((,class (:foreground ,const))))
    `(font-lock-doc-face ((,class (:foreground ,comment))))
-   `(font-lock-function-name-face ((,class (:foreground ,func ))))
+   `(font-lock-function-name-face ((,class (:bold t :foreground ,func ))))
    `(font-lock-keyword-face ((,class (:bold ,class :foreground ,keyword))))
    `(font-lock-string-face ((,class (:foreground ,str))))
    `(font-lock-type-face ((,class (:foreground ,type ))))
